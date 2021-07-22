c0e90e44de96753a7cdcd3b9e4723f89c9bf54df
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-07-22 15:37:32 +0900

    Add Command

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'plugin:prettier/recommended',
+    'prettier',
   ],
-};
+}
