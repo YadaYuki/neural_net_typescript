323b1d13356af69f936a2e18e99ea461891b8ac1
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-08-23 22:45:39 +0900

    Add loadMnist

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-// import { loadMnist } from './data/load-mnist';
-import fs from 'fs';
-import nj from 'numjs';
+import { loadMnist } from './data/load-mnist';
 
-// loadMnist();
-fs.readFile('train-images-idx3-ubyte', (err, data) => {
-  if (err) throw err;
-  console.log(data.slice(4, 8)); //
-  const offset = 16;
-  console.log((data.length - offset) / 784);
-  const imgSize = [28, 28];
-  // console.log(Array.from(data.slice(offset, offset + 784)));
-  console.log(
-    nj
-      .array(Array.from(data.slice(offset, offset + 784)))
-      .reshape(...imgSize)
-      .get(14, 14)
-    // .get(0)
-  );
-});
+loadMnist();
