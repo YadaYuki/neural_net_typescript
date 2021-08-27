9f6cad1882021aa00a1f36c57be6623f17db3f4a
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-08-28 01:41:31 +0900

    Add Batch

--- src/layers/relu.test.ts
+++ src/layers/relu.test.ts
@@ -3,15 +3,28 @@ import nj from 'numjs';
 
 describe('Relu Layer Test', () => {
   describe('Relu.forward', () => {
-    test('', () => {
+    test('forward', () => {
       const relu = new Relu();
       const x = nj.array([-1, 1, 2, 3]);
       expect(relu.forward(x).tolist()).toEqual([0, 1, 2, 3]);
     });
-    test('', () => {
+    test('forward', () => {
       const relu = new Relu();
       const x = nj.array([-1, 1, -2, 0]);
-      expect(relu.forward(x).tolist()).toEqual([0, 3, 0, 0]);
+      expect(relu.forward(x).tolist()).toEqual([0, 1, 0, 0]);
+    });
+  });
+  describe('Relu.forwardBatch', () => {
+    test('forwardBatch', () => {
+      const relu = new Relu();
+      const x = nj.array([
+        [-1, 1, -2, 0],
+        [-1, 1, -2, 0],
+      ]);
+      expect(relu.forwardBatch(x).tolist()).toEqual([
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ]);
     });
   });
 });
--- src/layers/relu.ts
+++ src/layers/relu.ts
@@ -2,10 +2,19 @@ import { Layer } from './base';
 import nj from 'numjs';
 
 export class Relu implements Layer {
-  forward = (x: nj.NdArray): nj.NdArray => {
-    return nj.array([0, 1, 2, 3]);
+  forward = (x: nj.NdArray<number>): nj.NdArray<number> => {
+    const xArray = x.tolist();
+    return nj.array(xArray.map((x) => (x > 0 ? x : 0)));
   };
 
+  forwardBatch = (x: nj.NdArray<number[]>): nj.NdArray<number[]> => {
+    const xArray = x.tolist();
+    return nj.array(
+      xArray.map((xArr: number[]) => {
+        return xArr.map((x) => (x > 0 ? x : 0));
+      })
+    );
+  };
   backward = (): void => {
     1 + 1;
   };
