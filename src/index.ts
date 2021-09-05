8987bff3119f924a35c74d3871f59708372ce834
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-09-05 12:15:48 +0900

    Add README.md

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ const main = async () => {
   const batchSize = 100;
   const iterNums = 1000;
   const learningRate = 0.1;
-  const start = Date.now();
   for (let i = 0; i < iterNums; i++) {
     const batchIdxList = choice(trainNum, batchSize);
     const xBatch = getBatchData(batchIdxList, xTrain);
@@ -19,9 +18,9 @@ const main = async () => {
     network.backward(); // 逆伝搬
     network.update(learningRate); // パラメータの更新
     if (i % 100 === 0) {
-      const now = Date.now();
-      console.log(now - start); // 実行にかかる時間を表示.
+      console.log(`iteration: ${i + 1}`);
       console.log(loss); // 誤差関数の出力結果を表示.
+      console.log();
     }
   }
   // テストデータを対象に精度を計算する。
